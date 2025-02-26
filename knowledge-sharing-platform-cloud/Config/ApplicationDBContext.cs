956a7f2078effe0981191ebdc81c958f269fb84d
PUIYOUMIN <youminpui@gmail.com>
2025-02-26 17:14:27 +0800

    Add project files.

---
+++ knowledge-sharing-platform-cloud/Config/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using knowledge_sharing_platform_cloud.Entity;
+using System.Data;
+using System.Reflection;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace knowledge_sharing_platform_cloud.config
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ knowledge-sharing-platform-cloud/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace knowledge_sharing_platform_cloud.Entity
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Email { get; set; }
+
+
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+
+        public string? Profile_Url { get; set; }
+    }
+}
